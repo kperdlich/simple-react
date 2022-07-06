11bd9e4632b88d01d48e5ee0f9fbf0eb21f6a05f
Kevin Perdlich <kevin@oak.tt>
2022-07-06 18:43:08 +0200

    Dog Dummy component

---
+++ src/component/DogList.tsx
@@ -0,0 +1,41 @@
+import {useState} from "../react/Hooks";
+
+interface DogItemProps {
+    readonly src: string;
+    readonly index: number;
+    readonly onDelete: (index: number) => void;
+}
+
+export const DogItem = ({src, onDelete, index}: DogItemProps) => {
+    return (
+        <img className={"image"} key={src} src={src} onClick={() => onDelete(index)}/>
+    );
+}
+
+export const DogList = () => {
+    const [dogs, setDogs] = useState<string[]>([]);
+
+    const fetchDog = async () => {
+        try {
+            const response = await fetch("https://dog.ceo/api/breeds/image/random");
+            const newDog = await response.json();
+            setDogs((dogs) => [...dogs, newDog.message]);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    const onDeleteDog = (index: number) => {
+        setDogs((dogs) => dogs.filter((_, idx) => index !== idx));
+    }
+
+
+    return (
+        <div>
+            <button onClick={fetchDog}>Add new Dog</button>
+            <ul>
+                {dogs.map((dog, index) => <DogItem key={dog} src={dog} index={index} onDelete={onDeleteDog}/>)}
+            </ul>
+        </div>
+    );
+}
